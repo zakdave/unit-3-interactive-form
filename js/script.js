0b5e95c62b80dad6e27fb45efce4e997e7f62680
zakdave <74794944+zakdave@users.noreply.github.com>
2021-01-07 14:33:22 -0500

    tshirt features

--- js/script.js
+++ js/script.js
@@ -1,24 +1,45 @@
-//Declaration of variables
-const nameField = document.querySelector('#name');
+// Declaration of variables
+const nameInput = document.querySelector('#name');
 const jobRoles = document.querySelector('#title');
 const otherJobRole = document.querySelector('#other-job-role');
+const selectDesign = document.querySelector('#design');
+const designChildren = selectDesign.children;
+const selectColor = document.querySelector('#color');
+const colorChildren = selectColor.children;
 
-
-otherJobRole.disabled = true;
-
-
+if(colorChildren[0].value === 'tomato') {
+    console.log(colorChildren[0].value);
+}
 
 //Set focus to the name field when page is loaded
-nameField.focus();
+nameInput.focus();
 
-//Working area for job role feature. Hide 'other' field when not selected and show when selected only
-otherJobRole.style.display = 'none'; // set to hide by default
+// Working area for job role feature. Hide 'other' field when not selected and show when selected only
+otherJobRole.hidden = true; // set to hide by default
 jobRoles.addEventListener('click', (e) => {
     const click = e.target;
     if (click.value === 'other' ) {
-        otherJobRole.style.display = 'block';
+        otherJobRole.hidden = false;
     } else {
-        otherJobRole.style.display = 'none';
+        otherJobRole.hidden = true;
     }
 });
 
+// Working area for T-shirt features 
+// Add event listener for design menu and deselect all unavailable options, set attribute to selected
+selectDesign.addEventListener('click', (e) => {
+    const click = e.target;
+    if (click.value === 'js puns') {
+        for (let i = 0; i < colorChildren.length; i++){
+            if (colorChildren[i].value === 'tomato'|| colorChildren[i].value === 'steelblue'|| colorChildren[i].value === 'dimgrey') {
+                colorChildren[i].setAttribute('disabled', '');
+            }
+        }
+    } else if (click.value = 'heart js') {
+        for (let i = 0; i < colorChildren.length; i++){
+            if (colorChildren[i].value === 'cornflowerblue'|| colorChildren[i].value === 'darkslategrey'|| colorChildren[i].value === 'gold') {
+                colorChildren[i].setAttribute('disabled', '');
+            }
+        }
+    }
+});
