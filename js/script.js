ccb52b0f3267555f3c54c0a68607b253fdc9bd63
zakdave <74794944+zakdave@users.noreply.github.com>
2021-01-08 11:38:56 -0500

    activities section

--- js/script.js
+++ js/script.js
@@ -2,10 +2,16 @@
 const nameInput = document.querySelector('#name');
 const jobRoles = document.querySelector('#title');
 const otherJobRole = document.querySelector('#other-job-role');
+
 const selectDesign = document.querySelector('#design');
 const designChildren = selectDesign.children;
 const selectColor = document.querySelector('#color');
 const colorChildren = selectColor.children;
+
+const activitiesFieldset = document.querySelector('#activities');
+const activitiesInput = document.querySelectorAll('input[type="checkbox"]');
+const pricePTag = activitiesFieldset.querySelector('p');
+let totalPrice = 0;
 
 if(colorChildren[0].value === 'tomato') {
     console.log(colorChildren[0].value);
@@ -31,15 +37,34 @@ selectDesign.addEventListener('click', (e) => {
     const click = e.target;
     if (click.value === 'js puns') {
         for (let i = 0; i < colorChildren.length; i++){
-            if (colorChildren[i].value === 'tomato'|| colorChildren[i].value === 'steelblue'|| colorChildren[i].value === 'dimgrey') {
-                colorChildren[i].setAttribute('disabled', '');
+            if (i === 4|| i === 5|| i === 6) {
+                colorChildren[i].hidden = true;
+            } else {
+                colorChildren[i].hidden = false;
             }
         }
-    } else if (click.value = 'heart js') {
+    }
+    if (click.value === 'heart js') {
         for (let i = 0; i < colorChildren.length; i++){
-            if (colorChildren[i].value === 'cornflowerblue'|| colorChildren[i].value === 'darkslategrey'|| colorChildren[i].value === 'gold') {
-                colorChildren[i].setAttribute('disabled', '');
+            if (i === 1|| i === 2|| i === 3) {
+                colorChildren[i].hidden = true;
+            } else {
+                colorChildren[i].hidden = false;
             }
         }
     }
 });
+
+//Working area for register activities section
+activitiesFieldset.addEventListener('change', (e) => {
+    const clicked = e.target;
+    const cost = +clicked.getAttribute('data-cost');
+    if (clicked.checked) {
+        totalPrice += cost;
+        console.log(totalPrice);
+    } else if (!clicked.checked) {
+        totalPrice -= cost;
+        console.log(totalPrice);
+    }
+    pricePTag.innerHTML = `Total: $${totalPrice}`;
+})
