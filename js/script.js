e416bb86f0f5ae71456e7afee8a9ca32ed188075
zakdave <74794944+zakdave@users.noreply.github.com>
2021-01-12 09:30:33 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -107,41 +107,110 @@ paymentMethod.addEventListener('change', (e) => {
 
 });
 
-// Working area for validation
+// Working area for validation, remove console log statements when complete
+const validationPass = (element) => {
+    element.parentElement.className = 'valid';
+    element.parentElement.classList.remove('not-valid');
+    element.parentElement.lastElementChild.hidden = true;
+};
+
+const validationFail = (element) => {
+    element.parentElement.className = 'not-valid';
+    element.parentElement.classList.remove('valid');
+    element.parentElement.lastElementChild.hidden = false;
+  };
+
 const nameValidator = () => {
     const name = nameInput.value;
     console.log("Name value is: ", `"${name}"`);
-
     const nameIsValid = name.length > 0;
     console.log(`Name validation test on "${name}" evaluates to ${nameIsValid}`);
-
+    if (nameIsValid) {
+        validationPass(nameInput);
+    } else {
+        validationFail(nameInput);
+    };
     return nameIsValid;
 };
 
 const emailValidator = () => {
     const email = emailInput.value;
     console.log("Email value is: ", `"${email}"`);
-
     const emailIsValid = /^[^@]+@[^@.]+\.[a-z]+$/i.test(email);
     console.log(`Email validation test on "${email}" evaluates to ${emailIsValid}`);
-
+    if (emailIsValid) {
+        validationPass(emailInput);
+    } else {
+        validationFail(emailInput);
+    };
     return emailIsValid;
 };
 
 const activitiesValidator = () => {
-    // continue working here
-    return nameIsValid;
+    if (numActivities > 0) {
+        validationPass(document.querySelector('#activities-box'));
+    } else {
+        validationFail(document.querySelector('#activities-box'));
+    }
+    return numActivities > 0;
 };
 
-// Add event handler for submit button
-form.addEventListener('submit', e => {
-    e.preventDefault();
+const cardNumValidator = () => {
+    const creditCardNum = creditCardNumInput.value;   
+    creditCardIsValid = /^\d{13,16}$/.test(creditCardNum);
+    console.log("cc value is: ", `"${creditCardNum}" and evaluates to ${creditCardIsValid}`);
+    if (creditCardIsValid) {
+        validationPass(creditCardNumInput);
+    } else {
+        validationFail(creditCardNumInput);
+    };
+    return creditCardIsValid;
+};
 
-    if (nameValidator()){
+const zipCodeValidator = () => {
+    const zipCode = zipCodeInput.value;
+    zipCodeIsValid = /^\d{5}$/.test(zipCode);
+    console.log(`zip is ${zipCode} and zipCodeIsValid returns ${zipCodeIsValid}`);
+    if (zipCodeIsValid) {
+        validationPass(zipCodeInput);
+    } else {
+        validationFail(zipCodeInput);
+    };
+    return zipCodeIsValid;
+};
+
+const CVVValidator = () => {
+    const cvv = CVVInput.value;
+    cvvIsValid = /^\d{3}$/.test(cvv);
+    console.log(`cvv is ${cvv} and cvvIsValid returns ${cvvIsValid}`);
+    if (cvvIsValid) {
+        validationPass(CVVInput);
+    } else {
+        validationFail(CVVInput);
+    };
+    return cvvIsValid;
+};
+
+// Event handler for submit button
+form.addEventListener('submit', e => {
+
+    if (!nameValidator()) {
         e.preventDefault();
     };
-    if (emailValidator()){
+    if (!emailValidator()) {
         e.preventDefault();
     };
-    console.log('Submit works.')
+    if (!activitiesValidator()) {
+        e.preventDefault();
+    };
+    if (!cardNumValidator()) {
+        e.preventDefault();
+    };
+    if (!zipCodeValidator()) {
+        e.preventDefault();
+    };
+    if (!CVVValidator()) {
+        e.preventDefault();
+    };
+    console.log('Submit works.');
 });
